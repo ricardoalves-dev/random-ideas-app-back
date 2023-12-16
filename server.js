e2b65d8b06879316b8290ac1b5f661a866e48782
ricardoalves-dev <ricardo-j-alves@hotmail.com>
2023-12-16 09:22:15 -0300

    Utilizar router /api/ideas

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const HttpStatus = require('./HttpStatus.js');
 const porta = 5000;
-
-app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
+const ideasRouter = require('./routes/ideas.js');
 
 app.get('/ping', (req, res) => {
   res.status(HttpStatus.OK).send('pong');
 });
+
+app.use('/api/ideas', ideasRouter);
+
+app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
