62f6cb15987367dd70d1eb97c1ea386eaa62b013
ricardoalves-dev <ricardo-j-alves@hotmail.com>
2023-12-16 09:07:55 -0300

    Criar objeto simulando os dados a serem buscados

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    id: 1,
+    autor: 'Tony Stark',
+    descricao: 'Salvar o mundo',
+  },
+  {
+    id: 2,
+    autor: 'Steve Rodgers',
+    descricao: 'Lutar pela paz',
+  },
+];
+
+module.exports = data;
