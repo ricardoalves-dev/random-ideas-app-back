897f2f5dabdf903f2797125bdced308061aa639b
ricardoalves-dev <ricardo-j-alves@hotmail.com>
2023-12-18 21:07:31 -0300

    Adicionar middlewares para parse do body

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/ping', (req, res) => {
   res.status(HttpStatus.OK).send('pong');
 });
 
+// Middlewares
+app.use(express.json()); // Este middleware é responsável por fazer o parsing (análise) do corpo das requisições com formato JSON. Se você não utilizar este middleware, o req.body não estará disponível para requisições que enviam dados no formato JSON
+app.use(express.urlencoded({extended: false})); // Semelhante ao acima, porém para o formato application/x-www-form-urlencoded
 app.use('/api/ideas', ideasRouter);
 
+// Levantar API
 app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
