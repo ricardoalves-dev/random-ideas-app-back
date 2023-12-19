1b687eb91597d0d3c3e82051f44b8f961b65f3b4
ricardoalves-dev <ricardo-j-alves@hotmail.com>
2023-12-19 20:50:28 -0300

    Melhorias no server.js

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const app = express();
-const HttpStatus = require('./HttpStatus.js');
-const porta = 5000;
 const ideasRouter = require('./routes/ideas.js');
+require('dotenv').config(); // Este pacote carregar os valores do .env para o objeto process.env
+const conectarDB = require('./config/db.js');
 
-app.get('/ping', (req, res) => {
-  res.status(HttpStatus.OK).send('pong');
-});
+function configurarMiddlewares(){  
+  app.use(express.json()); // Este middleware é responsável por fazer o parsing (análise) do corpo das requisições com formato JSON. Se você não utilizar este middleware, o req.body não estará disponível para requisições que enviam dados no formato JSON
+  app.use(express.urlencoded({extended: false})); // Semelhante ao acima, porém para o formato application/x-www-form-urlencoded
+}
 
-// Middlewares
-app.use(express.json()); // Este middleware é responsável por fazer o parsing (análise) do corpo das requisições com formato JSON. Se você não utilizar este middleware, o req.body não estará disponível para requisições que enviam dados no formato JSON
-app.use(express.urlencoded({extended: false})); // Semelhante ao acima, porém para o formato application/x-www-form-urlencoded
-app.use('/api/ideas', ideasRouter);
+function configurarRotas(){
+  app.get('/ping', (req, res) => {
+    res.status(HttpStatus.OK).send('pong');
+  });
 
-// Levantar API
-app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
+  app.use('/api/ideas', ideasRouter);
+}
+
+function iniciar(){ 
+
+  try {
+    configurarMiddlewares();
+    configurarRotas();  
+    conectarDB(process.env.MONGO_URI);
+    app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`));  
+  } catch (error) {
+    console.log(`Erro ao iniciar a API: ${error}`);
+  }  
+}
+
+iniciar();
